bef25cdea8b9f2fa43d8ca5b5abb4cbcb374539f
Pasang-lama <pasanglama66666@gmail.com>
2024-03-14 12:06:55 +0545

    works on slider and gallery images

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.limited-edition-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="bi bi-arrow-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="bi bi-arrow-right"></i></button>',
+    });
+  });
